d9515a12f1d88fdf17fc76a1f3a80db47a60f8c9
Borislav <bonev@inout.bg>
2021-11-07 22:06:30 +0200

    Daily recipes section - Done.

--- src/components/main-section-elements/DailyRecipes.js
+++ src/components/main-section-elements/DailyRecipes.js
@@ -1,3 +1,5 @@
+import RecipeItemNormal from  "../recipeItems/RecipeItemNormal.js"
+import RecipeItemBig from  "../recipeItems/RecipeItemBig.js"
 
 export default function DailyRecipes(){
 
@@ -6,49 +8,29 @@ export default function DailyRecipes(){
           <div className="container">
 
             <div className="section-title">
-              <h2>Deily Recipes</h2>
+              <h2>Daily Recipes</h2>
               <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus eum molestiae sunt consectetur ducimus nulla harum voluptate nobis quae iure!</p>
             </div>
 
-            <div className="row">
-              <div className="col-lg-4 col-md-6 d-flex">
+            <div className="row d-flex justify-content-center">
+
+              <div className="col-lg-3 d-recipe-left-column">
                 <div className="row">
-                  <div class="col-lg-12 col-md-6 d-flex align-items-stretch recipe-item">
-                      <div class="recipe">
-                        <div class="recipe-img pb-3 pt-2">
-                          
-                          <img src="https://via.placeholder.com/450" class="img-fluid" alt="" />
-                        </div>
-                        <div class="recipe-info pb-3">
-                          <h5>Lorem, ipsum.</h5>
-                          <span>
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-                            Explicabo libero necessitatibus sequi quo reiciendis fugit excepturi animi officia nam ipsum?
-                          </span>
-                        </div>
-                        <div className="recipe-footer pb-5">
-                          <div className="recipe-rating float-start">
-                            <span className="fa fa-star checked"></span>
-                            <span className="fa fa-star checked"></span>
-                            <span className="fa fa-star checked"></span>
-                            <span className="fa fa-star"></span>
-                            <span className="fa fa-star"></span>
-                            <span className="ms-2">3.5</span>
-                          </div>
-                          <div className="recipe-author float-end">
-                              by <a href="/" target="_blank" rel="noopener noreferrer">Test</a>
-                          </div>
-                        </div>
-                       
-                      </div>
-                  </div>
+
+                  <RecipeItemNormal category="Breakfast"/>
+                  <RecipeItemNormal category="Lunch"/>
+
                 </div>
-             
               </div>
-              
+
               <div className="col-lg-8">
-               <p>21312</p>
+                <div className="row d-recipe-left-el">
+
+                  <RecipeItemBig />
+
+                </div>
               </div>
+
             </div>
           </div>
         </section>
---
+++ src/components/recipeItems/RecipeItemNormal.js
@@ -0,0 +1,40 @@
+
+export default function RecipeItemNormal(props)
+{
+    return (
+        <div className="col-lg-12 col-md-6 d-flex align-items-stretch recipe-item mt-2">
+
+            <div className="recipe">
+                <div className="recipe-title pt-2">
+                    Category: {props.category}
+                </div>
+                <div className="recipe-img pb-3 pt-2">
+                    <img src="https://via.placeholder.com/330x200" className="img-fluid" alt="" />
+                </div>
+
+                <div className="recipe-info pb-3">
+                    <h5>Lorem, ipsum.</h5>
+                    <span>
+                    Lorem ipsum dolor sit amet consectetur.
+                    </span>
+                </div>
+
+                <div className="recipe-footer pb-5">
+                    <div className="recipe-rating float-start">
+                    <span className="fa fa-star checked"></span>
+                    <span className="fa fa-star checked"></span>
+                    <span className="fa fa-star checked"></span>
+                    <span className="fa fa-star"></span>
+                    <span className="fa fa-star"></span>
+                    <span className="ms-2">3.5</span>
+                    </div>
+                    <div className="recipe-author float-end">
+                        by <a href="/" target="_blank" rel="noopener noreferrer">Test</a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
+
+}
